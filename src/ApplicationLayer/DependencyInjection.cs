9f194d357b90e18107b9f005a7af3d10f9b8989f
gycsaba <gycsaba@SZT9-24>
2025-02-04 11:32:24 +0100

    ApplicationLayer

---
+++ src/ApplicationLayer/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ApplicationLayer
+{
+    public  class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assambly = typeof(DependencyInjection).Assembly;
+            services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(assambly));
+            return services;
+        }
+    }
+}
