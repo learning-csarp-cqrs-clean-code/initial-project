bbc2e2d11727869a99abab557bb14fff58d463e7
gycsaba <gycsaba@SZT9-24>
2025-02-04 14:00:13 +0100

    Application és Infrastructure réteg

--- src/ApplicationLayer/DependencyInjection.cs
+++ src/ApplicationLayer/DependencyInjection.cs
@@ -2,7 +2,7 @@
 
 namespace ApplicationLayer
 {
-    public  class DependencyInjection
+    public static class DependencyInjection
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
